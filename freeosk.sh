7876b737e757951bebee7be2eaf0020020d3018f
David Saenz Tagarro <david@redradix.com>
2014-07-15 17:51:56 +0200

    Added redis-server window to freeosk

--- freeosk.sh
+++ freeosk.sh
@@ -1,17 +1,20 @@
 project="freeosk"
+mysql.server start
 cd ~/Development/projects/freeosk
 
 tmux new-session -d -s $project
 tmux rename-window 'vim'
 tmux new-window -t $project:0 -n 'vim'
 tmux new-window -t $project:1 -n 'console'
-tmux new-window -t $project:2 -n 'server'
-tmux new-window -t $project:3 -n 'my-notes'
+tmux new-window -t $project:2 -n 'rails-server'
+tmux new-window -t $project:3 -n 'redis-server'
+tmux new-window -t $project:4 -n 'my-notes'
 
 tmux send-keys -t $project:0 "vim" Enter
 tmux send-keys -t $project:1 "rails c" Enter
-# tmux send-keys -t $project:2 "rails s" Enter
-tmux send-keys -t $project:3 'vim ~/Development/my-notes/freeosk.md' Enter
+tmux send-keys -t $project:2 "rails s" Enter
+tmux send-keys -t $project:3 'redis-server' Enter
+tmux send-keys -t $project:4 'vim ~/Development/my-notes/freeosk.md' Enter
 
 # init focus
 tmux attach -t $project:0
